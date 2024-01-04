29641a47500bffc9771eb6565f78d7caa56e0f2f
spectre <nathancraigforsyth@gmail.com>
2024-01-05 12:09:06 +1300

    Update swagger doc contact details

--- Petroliq/Program.cs
+++ Petroliq/Program.cs
@@ -66,7 +66,7 @@ namespace Petroliq
                     Contact = new OpenApiContact
                     {
                         Name = "Dreamsof.dev",
-                        Url = new Uri("https://dreamsof.dev/t")
+                        Url = new Uri("https://dreamsof.dev/")
                     },
                     License = new OpenApiLicense
                     {
